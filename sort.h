89cd376dec586e060d42af13b321b939c8bf4d95
VU DUY KHA <24127178@student.hcmus.edu.vn>
2025-03-15 10:58:45 +0700

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,34 +1,41 @@
 #ifndef SORT_H
 #define SORT_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <fstream>  
 
-void selectionSort(int arr[], int n, int& countCmp);
+using namespace std;
 
-void bubbleSort(int arr[], int n, int& countCmp);
+void selectionSort(int arr[], int n, long long& countCmp);
 
-void merge (int arr[], int left, int mid, int right, int& countCmp);
-void mergeSort (int arr[], int left, int right, int& countCmp);
+void bubbleSort(int arr[], int n, long long& countCmp);
 
-int binarySearch(int a[], int item, int left, int right, int& countCmp);
-void binaryInsertionSort(int a[], int n, int& countCmp);
+void merge (int arr[], int left, int mid, int right, long long& countCmp);
+void mergeSort (int arr[], int left, int right, long long& countCmp);
 
-void insertionSort(int arr[], int n, int& countCmp);
+int binarySearch(int a[], int item, int left, int right, long long& countCmp);
+void binaryInsertionSort(int a[], int n, long long& countCmp);
 
-int partition(int arr[], int low, int high, int& countCmp);
-void quickSort(int arr[], int low, int high,int& countCmp);
+void insertionSort(int arr[], int n, long long& countCmp);
 
-void countingSort(int arr[], int n, int& countCmp);
+int partition(int arr[], int low, int high, long long& countCmp);
+void quickSort(int arr[], int low, int high,long long& countCmp);
 
-void flashSort(int arr[], int n,int& countCmp);
+void countingSort(int arr[], int n, long long& countCmp);
 
-void shellSort(int a[], int n, int& countCmp);
+void flashSort(int arr[], int n,long long& countCmp);
 
-void shakerSort(int a[], int n, int& countCmp);
+void shellSort(int a[], int n, long long& countCmp);
 
-void heapRebuild(int i, int a[], int n, int& countCmp);
-void heapConstruct(int a[], int n, int& countCmp);
-void heapSort(int a[], int n, int& countCmp);
+void shakerSort(int a[], int n, long long& countCmp);
 
-void radixSort(int a[], int n, int& countCmp);
+void heapRebuild(int i, int a[], int n, long long& countCmp);
+void heapConstruct(int a[], int n, long long& countCmp);
+void heapSort(int a[], int n, long long& countCmp);
+
+void radixSort(int a[], int n, long long& countCmp);
 
 void GenerateRandomData(int a[], int n);
 void GenerateSortedData(int a[], int n);
@@ -37,8 +44,8 @@ void GenerateNearlySortedData(int a[], int n);
 
 void GenerateData(int a[], int n, int dataType);
 
-double measureSortingTime(string algorithm, int arr[], int n, int& countCmp);
-void writeResults(string algorithm, string order, int size, double time, int countCmp);
+double measureSortingTime(string algorithm, int arr[], int n, long long& countCmp);
+void writeResults(string algorithm, string order, int size, double time, long long countCmp);
 void runExperiments();
 
 #endif
